5a0d3c5c982bcefbe78314c0cf760f95a98a311c
Hafiz Wildan Ahmed Siddiqui <160965922+hafizsiddiqui1211@users.noreply.github.com>
2025-02-25 08:20:43 +0500

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,7 @@
+sentence = input("Enter a sentence: ")  # Input: "Python is amazing!""
+print(len(sentence.split()))  # Output: 3
+words = sentence.split()
+print(len(words))  # Output: 3
+Length = len(words)
+print(f"Total Words: {Length}")  # Output: 3
+print(sentence[::-1])  # Output: "!gnizama si nohtyP"
